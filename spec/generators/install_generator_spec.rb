f2b44e598283b36729c51a7a78460c880f12a891
Baylor Rae <baylor@thecodedeli.com>
2019-02-09 12:39:49 -0600

    add complete initializer

--- spec/generators/install_generator_spec.rb
+++ spec/generators/install_generator_spec.rb
@@ -1,19 +1,37 @@
 require "rails_helper"
 require "event_sourcery/rails/generators/install_generator"
 
+module MyApplication
+  App = Class.new
+end
+
 module EventSourceryRails
   describe InstallGenerator, type: :generator do
     let(:initializer_file) { file('config/initializers/event_sourcery.rb') }
 
-    it "adds to the rails application name" do
+    around do |example|
       orig_app = Rails.application
-
-      stub_const('MyApplication::App', Class.new)
       Rails.application = MyApplication::App.new
-      generator = run_generator %w(event_sourcery_rails:install)
-      expect(initializer_file).to contain("module MyApplication")
-
+      example.run
       Rails.application = orig_app
+    end
+
+    context "copy_initializer" do
+      before do
+        generator = run_generator %w(event_sourcery_rails:install)
+      end
+
+      it "adds to the rails application name" do
+        expect(initializer_file).to contain("module MyApplication")
+      end
+
+      it "configures the application" do
+        expect(initializer_file).to contain("MyApplication.configure do |config|")
+      end
+
+      it "connects with application database_url" do
+        expect(initializer_file).to contain("Sequel.connect(MyApplication.config.database_url)")
+      end
     end
   end
 end
